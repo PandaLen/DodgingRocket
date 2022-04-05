ff2ccce66f7b9ba23dcfce5e51d7af476f569db7
PandaLen <lentocz@gmail.com>
2022-04-05 20:36:34 +0200

    Náhodné generování meteoritů + kolize

--- public/js/classes.js
+++ public/js/classes.js
@@ -1,36 +1,66 @@
-let rocketSprite;
+let rocketSprite, asteroidSprite;
 
 function preload() {
-    rocketSprite = loadImage("../img/rocket.png");
-  }
+  rocketSprite = loadImage("../img/rocket.png");
+  asteroidSprite = loadImage("../img/meteor.png");
+}
 
 class Rocket {
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-        this.w = 120;
-        this.h = 70;
-    }
-    draw(){
-        rect(this.x, this.y, this.w, this.h);
-        noFill();
-        image(rocketSprite.get(0, 0, this.w, this.h), this.x, this.y);
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.w = 120;
+    this.h = 70;
+  }
+  detectCollision(asteroid) {
+    return collideRectRect(
+      this.x,
+      this.y,
+      this.w,
+      this.h,
+      asteroid.x,
+      asteroid.y,
+      asteroid.size,
+      asteroid.size
+    );
+  }
+  draw() {
+    rect(this.x, this.y, this.w, this.h);
+    noFill();
+    image(rocketSprite.get(0, 0, this.w, this.h), this.x, this.y);
 
-        // Pohyb nahoru
-        if(keyIsDown(87)){
-            if (this.y > 0) this.y -= 7;
-        }
-        // Pohyb doprava
-        if (keyIsDown(68)) {
-            if (this.x < width - this.w) this.x += 7;
-          }
-          // Pohyb dolů
-          if (keyIsDown(83)) {
-            if (this.y < height - this.h) this.y += 7;
-          }
-        // Pohyb doleva
-        if (keyIsDown(65)) {
-            if (this.x > 0) this.x -= 7;
-          }
+    // Pohyb nahoru
+    if (keyIsDown(87)) {
+      if (this.y > 0) this.y -= 7;
     }
+    // Pohyb doprava
+    if (keyIsDown(68)) {
+      if (this.x < width - this.w) this.x += 7;
+    }
+    // Pohyb dolů
+    if (keyIsDown(83)) {
+      if (this.y < height - this.h) this.y += 7;
+    }
+    // Pohyb doleva
+    if (keyIsDown(65)) {
+      if (this.x > 0) this.x -= 7;
+    }
+  }
+}
+
+class Asteroid {
+  constructor() {
+    this.size = 130;
+    this.y = random(this.size, height - this.size);
+    this.x = width + 100;
+    this.speed = random(5, 10);
+  }
+  move() {
+    this.x -= this.speed;
+  }
+  draw() {
+    rect(this.x, this.y, this.size, this.size);
+    this.move();
+    image(asteroidSprite.get(0, 0, this.size, this.size), this.x, this.y);
+  }
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,8 @@
 let canvas, rocket;
+let time = 0;
+let score = 0;
+let hp = 3;
+let asteroids = [];
 
 function centerCanvas() {
     x = (windowWidth - width) / 2;
@@ -13,6 +17,24 @@ function setup() {
 
 function draw() {
     centerCanvas();
-    canvas.background(color(0,0,0));
+    time++;
+    canvas.background(color(0, 0, 0));
     rocket.draw();
+    if (time % 50 == 0) {
+        asteroids.push(new Asteroid());
+    }
+    asteroids.forEach(function (asteroid, index, array) {
+        asteroid.draw();
+        if (rocket.detectCollision(asteroid)) {
+            array.splice(index, 1);
+            if (hp > 0) {
+                hp--;
+                console.log(hp);
+            }
+        }
+        if (asteroid.x < -150) {
+            array.splice(index, 1);
+            score++;
+        }
+    });
 }
